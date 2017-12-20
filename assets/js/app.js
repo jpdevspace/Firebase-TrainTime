be4ed7b9cf8959832e961e7c4ab70d8284093d90
jpwebdev <jjpwebdev@gmail.com>
2017-12-20 11:01:53 -0500

    Firebase connected, getting the elemens frmo the page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,25 @@
 $(function(){
     console.log('Working!');
+    // Initialize Firebase
+    const config = {
+        apiKey: "AIzaSyCn6bxidmbH6601u0islr5_xyteVr41dLI",
+        authDomain: "codersay-53fd9.firebaseapp.com",
+        databaseURL: "https://codersay-53fd9.firebaseio.com",
+        projectId: "codersay-53fd9",
+        storageBucket: "codersay-53fd9.appspot.com",
+        messagingSenderId: "878808016417"
+    };
+    firebase.initializeApp(config);
+
+    // Reference Firebase DB
+    const db = firebase.database();
+
+    // Get the elements
+    const $tName = $('#name');
+    const $tDest = $('#destination');
+    const $tFirstTime = $('#firstTrainTime');
+    const $tFreq = $('#frequency');
+
+
+
 })
