150ae2456610a4d9b979a4b413c5494d6f64c3b9
jpwebdev <jjpwebdev@gmail.com>
2017-12-20 22:49:21 -0500

    completed

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Train-Time
-Plan your train trip with this awesome Firebase train schedule
+Plan your train trip with this awesome Firebase train scheduler
+
+## You can see it live here: https://jpdevspace.github.io/Train-Time/.
+
+Technologies used for this project: Firebase, JavaScript, Bootstrap v4 and HTML
+
+![alt text][screenshot]
+
+[screenshot]: https://github.com/jpdevspace/Train-Time/blob/master/assets/imgs/screenshot.png "Game Screenshot"
+
+### INSTRUCTIONS
+Access the app and schedule your trains. It automatically generates the time for next arrival and waiting time until next train, depending on the information you provide (name, destination, first train time and frequency). Data is saved into Firebase database so the information will be there next time you visit.
--- assets/js/app.js
+++ assets/js/app.js
@@ -28,39 +28,53 @@ $(function(){
     // Functions
     const getValues = (e) => {
         e.preventDefault();
-        // Push values into an obj
+        // Time Calculation
+        // First time (pushed back 1 year)
+        let firstTimeConverted = moment($tFirstTime.val(), "hh:mm").subtract(1, "years");
+        // Current Time
+        let currentTime = moment();
+        // Difference between the times
+        let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        // Time apart (remainder)
+        let tRemainder = diffTime % $tFreq.val();
+        // Mins until train
+        let tMinsTillTrain = $tFreq.val() - tRemainder;
+        // Next Train
+        let nextTrain = moment(moment().add(tMinsTillTrain, "minutes")).format("hh:mm");
 
+        // Push values into an obj
         trainObj.tName = $tName.val();
         trainObj.tDest = $tDest.val();
-        trainObj.tFirstTime = $tFirstTime.val();
         trainObj.tFreq = $tFreq.val();
+        trainObj.tNextArr = nextTrain;
+        trainObj.tMinsAway = tMinsTillTrain;
+
         // Clear inputs
         $tName.val('');
         $tDest.val('');
         $tFirstTime.val('');
         $tFreq.val('');
+
         // Put it into Firebase
         db.ref().push(trainObj);
     }
 
     // Listen for changes in the database
     dbRef.on('child_added', (snapshot) => {
+        // Check what its being pushed to the Firebase DB
         console.log(snapshot.val());
         // Fill the table
         $tbody.append(`
             <tr>
                 <td>${snapshot.val().tName}</td>
                 <td>${snapshot.val().tDest}</td>
-                <td>${snapshot.val().tFirstTime}</td>
                 <td>${snapshot.val().tFreq}</td>
+                <td>${snapshot.val().tNextArr}</td>
+                <td>${snapshot.val().tMinsAway}</td>
             </tr>`);
     })
 
-
-
     // Event Listener
     $submit.on('click', getValues);
-
-
 
 })
